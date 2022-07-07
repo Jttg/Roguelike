45a4809975fef7f5a3cdb22218642c0a1943292e
Jttg <jonasttg@gmail.com>
2022-07-07 11:04:24 +0100

    Week 2 Finished. Includes "tiles" (walls/floor). Game Map.

--- main.py
+++ main.py
@@ -2,16 +2,21 @@ import tcod
 
 from engine import Engine
 from entity import Entity
-from game_map import GameMap
+#from game_map import GameMap
 from input_handlers import EventHandler
+from procgen import generate_dungeon
 
 
 def main() -> None:
-    screen_width = 80
-    screen_height = 50
+    screen_width = 180
+    screen_height = 100
 
-    map_width = 80
-    map_height = 45
+    map_width = 180
+    map_height = 100
+
+    room_max_size = 15
+    room_min_size = 6
+    max_rooms = 45
 
     tileset = tcod.tileset.load_tilesheet(
         "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
@@ -23,7 +28,14 @@ def main() -> None:
     npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), "@", (255, 255, 0))
     entities = {npc, player}
 
-    game_map = GameMap(map_width, map_height)
+    game_map = generate_dungeon(
+        max_rooms=max_rooms,
+        room_min_size=room_min_size,
+        room_max_size=room_max_size,
+        map_width=map_width,
+        map_height=map_height,
+        player=player
+    )
 
     engine = Engine(entities=entities, event_handler=event_handler, game_map=game_map, player=player)
 
